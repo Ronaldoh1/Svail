2627e30cdf57c398c572378540c7453ec8472173
Zack Zhu <thornpig@gmail.com>
2015-04-15 10:00:47 -0700

    add linkedIn login

--- Svail/SocialVeriViewController.m
+++ Svail/SocialVeriViewController.m
@@ -11,24 +11,26 @@
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 
+#import "AFHTTPRequestOperation.h"
+#import "LIALinkedInHttpClient.h"
+#import "LIALinkedInApplication.h"
+
 
 
 @interface SocialVeriViewController ()
 
 @property (nonatomic) BOOL isUserVerified;
+@property (nonatomic) LIALinkedInHttpClient *linkedIn;
 
 
 @end
 
 @implementation SocialVeriViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 }
-
-
-
-
 
 
 - (IBAction)onTwitterVerifyButtonTapped:(UIButton *)sender
@@ -109,7 +111,41 @@
 
 - (IBAction)onLinkedInVerifyButtonTapped:(UIButton *)sender
 {
+    [self.linkedIn getAuthorizationCode:^(NSString *code) {
+        [self.linkedIn getAccessToken:code success:^(NSDictionary *accessTokenData) {
+            [self dismissViewControllerAnimated:YES completion:nil];
+            NSString *accessToken = [accessTokenData objectForKey:@"access_token"];
+            [self requestMeWithToken:accessToken];
+        }                   failure:^(NSError *error) {
+            NSLog(@"Quering accessToken failed %@", error);
+        }];
+    }                      cancel:^{
+        NSLog(@"Authorization was cancelled by user");
+    }                     failure:^(NSError *error) {
+        NSLog(@"Authorization failed %@", error);
+    }];
 }
+
+
+- (void)requestMeWithToken:(NSString *)accessToken {
+    [self.linkedIn GET:[NSString stringWithFormat:@"https://api.linkedin.com/v1/people/~?oauth2_access_token=%@&format=json", accessToken] parameters:nil success:^(AFHTTPRequestOperation *operation, NSDictionary *result) {
+        NSLog(@"current user %@", result);
+    }        failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"failed to fetch current user %@", error);
+    }];
+}
+
+- (LIALinkedInHttpClient *)linkedIn {
+    LIALinkedInApplication *application = [LIALinkedInApplication
+                                   applicationWithRedirectURL:@"https://localhost"
+                                   clientId: @"75696l29jqbq3l"
+                                   clientSecret:@"YYBB2iDxC63LjOhU"
+                                   state:@"f**kRonAndMert"
+                                   grantedAccess:@[@"r_fullprofile", @"r_network"]];
+    
+    return [LIALinkedInHttpClient clientForApplication:application presentingViewController:nil];
+}
+
 
 
 @end
