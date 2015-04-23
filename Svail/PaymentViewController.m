aa754eb29cdc19090d9dfa0c744318ac270e5d81
Zack Zhu <thornpig@gmail.com>
2015-04-23 08:24:41 -0700

    to push to remote dev

--- Svail/PaymentViewController.m
+++ Svail/PaymentViewController.m
@@ -7,8 +7,6 @@
 //
 
 #import "PaymentViewController.h"
-#import <Stripe.h>
-#import <Stripe+ApplePay.h>
 
 @interface PaymentViewController ()
 
@@ -21,44 +19,44 @@
     // Do any additional setup after loading the view.
 
 
-    PKPaymentRequest *request = [Stripe
-                                 paymentRequestWithMerchantIdentifier:@"merchant.com.Svail"];
-    // Configure your request here.
+//    PKPaymentRequest *request = [Stripe
+//                                 paymentRequestWithMerchantIdentifier:@"merchant.com.Svail"];
+//    // Configure your request here.
     NSString *label = @"Premium Turkish Food";
     NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithString:@"10.00"];
-    request.paymentSummaryItems = @[
-                                    [PKPaymentSummaryItem summaryItemWithLabel:label
-                                                                        amount:amount]
-                                    ];
+//    request.paymentSummaryItems = @[
+//                                    [PKPaymentSummaryItem summaryItemWithLabel:label
+//                                                                        amount:amount]
+//                                    ];
+//
+//    if ([Stripe canSubmitPaymentRequest:request]) {
 
-    if ([Stripe canSubmitPaymentRequest:request]) {
-
-    } else {
+//    } else {
         // Show the user your own credit card form (see options 2 or 3)
-    }
+//    }
 
 
 
 }
 // ViewController.m
 
-- (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
-                       didAuthorizePayment:(PKPayment *)payment
-                                completion:(void (^)(PKPaymentAuthorizationStatus))completion {
-    /*
-     We'll implement this method below in 'Creating a single-use token'.
-     Note that we've also been given a block that takes a
-     PKPaymentAuthorizationStatus. We'll call this function with either
-     PKPaymentAuthorizationStatusSuccess or PKPaymentAuthorizationStatusFailure
-     after all of our asynchronous code is finished executing. This is how the
-     PKPaymentAuthorizationViewController knows when and how to update its UI.
-     */
-    //[self handlePaymentAuthorizationWithPayment:payment completion:completion];
-}
+//- (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
+//                       didAuthorizePayment:(PKPayment *)payment
+//                                completion:(void (^)(PKPaymentAuthorizationStatus))completion {
+//    /*
+//     We'll implement this method below in 'Creating a single-use token'.
+//     Note that we've also been given a block that takes a
+//     PKPaymentAuthorizationStatus. We'll call this function with either
+//     PKPaymentAuthorizationStatusSuccess or PKPaymentAuthorizationStatusFailure
+//     after all of our asynchronous code is finished executing. This is how the
+//     PKPaymentAuthorizationViewController knows when and how to update its UI.
+//     */
+//    //[self handlePaymentAuthorizationWithPayment:payment completion:completion];
+//}
 
-- (void)paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller {
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
+//- (void)paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller {
+//    [self dismissViewControllerAnimated:YES completion:nil];
+//}
 
 
 @end
