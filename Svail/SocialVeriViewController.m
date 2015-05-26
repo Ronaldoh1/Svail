514a4a5c3cc75b0c63280d6704f83b97913be687
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-05-26 01:26:30 -0400

    change text

--- Svail/SocialVeriViewController.m
+++ Svail/SocialVeriViewController.m
@@ -177,7 +177,7 @@ static float const kAlphaForButtonsIfNotVerified = 1.0;
         return;
     }
     
-    NSString *message = [NSString stringWithFormat:@"Please help your friend %@ complete a simple safety check for using Svail. If you think %@ is a trustworthy person, please reply this meesage with %@'s 10-digit cell phone number. If you think otherwise, please don't reply. Thanks!", [User currentUser].name, [User currentUser].name, [User currentUser].name];
+    NSString *message = [NSString stringWithFormat:@"Please help your friend %@ complete a simple safety check in order to Svail. If you think %@ is a trustworthy person, please reply this message with %@'s 10-digit cell phone number. Do not use any any punctuations eg. dashes, periods, parenthesis etc. If you think otherwise, please don't reply. Thank you!", [User currentUser].name, [User currentUser].name, [User currentUser].name];
     for (int i = (int)(self.currentUser.verification.references.count); i < 3; i++) {
         UITextField *textField = self.phoneNumberTextFields[i];
         if (![textField.text isEqualToString:@""]) {
