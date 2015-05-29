1c4b32a0bdd1415fda3169b48daa700ca2ed163a
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-05-28 22:28:01 -0700

    able to see edit profile after login

--- Svail/EditProfileViewController.m
+++ Svail/EditProfileViewController.m
@@ -41,13 +41,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //setting image to Navigation Bar's title
-    UILabel *titleView = (UILabel *)self.navigationItem.titleView;
-    titleView = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 30)];
-    titleView.font = [UIFont fontWithName:@"Noteworthy" size:20];
-    titleView.text = @"Profile";
-    titleView.textColor = [UIColor colorWithRed:21/255.0 green:137/255.0 blue:255/255.0 alpha:1.0];
-    [self.navigationItem setTitleView:titleView];
 
     [self initialSetUp];
 
@@ -57,6 +50,15 @@
 
 }
 -(void)initialSetUp{
+
+    //setting image to Navigation Bar's title
+    UILabel *titleView = (UILabel *)self.navigationItem.titleView;
+    titleView = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 30)];
+    titleView.font = [UIFont fontWithName:@"Noteworthy" size:20];
+    titleView.text = @"Profile";
+    titleView.textColor = [UIColor colorWithRed:21/255.0 green:137/255.0 blue:255/255.0 alpha:1.0];
+    [self.navigationItem setTitleView:titleView];
+
 
     //set the delegate for each textfield to self.
     [self setDelegatesForTextFields];
